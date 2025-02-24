ff23cc80431ee9fc7755ee014c504a3217da3ce2
Madison Hanberry <mhanberry1@gmail.com>
2025-02-24 01:44:29 -0800

    integrate the cart with the product page

---
+++ web/js/cart.js
@@ -0,0 +1,44 @@
+import { $ } from './common.js'
+
+class Cart {
+	#items
+
+	constructor() {
+		this.#items = JSON.parse(
+			localStorage.getItem('cart') || '{}'
+		)
+	}
+
+	getItems() {
+		return this.#items
+	}
+
+	addItem(stripeProductId, purchaseQuantity) {
+		this.#items[stripeProductId] = purchaseQuantity
+
+		localStorage.setItem('cart', JSON.stringify(this.#items))
+
+		if (!$('#cart-indicator')) return
+
+		$('#cart-indicator').textContent = Object.keys(this.#items.length)
+	}
+
+	removeItem(stripeProductId) {
+		delete this.#items[stripeProductId]
+
+		if (!$('#cart-indicator')) return
+
+		$('#cart-indicator').textContent = Object.keys(this.#items.length)
+	}
+
+	clearItems() {
+		this.#items = {}
+		localStorage.setItem('cart', '{}')
+
+		if (!$('#cart-indicator')) return
+
+		$('#cart-indicator').textContent = Object.keys(this.#items.length)
+	}
+}
+
+export default new Cart()
--- web/js/product.js
+++ web/js/product.js
@@ -1,8 +1,9 @@
 import { $, e } from './common.js'
 import { listProducts } from './api.js'
+import cart from './cart.js'
 
 const queryParams = new URLSearchParams(location.href.split('?')[1])
-const stripeProductId = JSON.parse(queryParams.get('stripeProductIds'))
+const stripeProductId = JSON.parse(queryParams.get('stripeProductIds'))[0]
 
 const {
 	images,
@@ -10,7 +11,7 @@ const {
 	default_price: { unit_amount },
 	quantity,
 	description,
-} = (await (await listProducts(stripeProductId)).json()).products[0]
+} = (await (await listProducts([stripeProductId])).json()).products[0]
 
 let imgIdx = 0
 
@@ -43,9 +44,23 @@ $('#purchase-quantity').onchange = () => {
 		: Math.min(quantity, currentVal)
 }
 
+$('#add-to-cart').onclick = () => {
+	cart.addItem(
+		stripeProductId,
+		$('#purchase-quantity').value,
+	)
+
+	$('#added-to-cart').classList.add('show')
+}
+
+if (cart.getItems()[stripeProductId]) {
+	$('#added-to-cart').classList.add('show')
+}
+
 $('title').textContent = `Stitch Cafe - ${name}`
 $('#current-image').href = images[imgIdx]
 $('#product-name').textContent = name
 $('#price').textContent = '$' + parseFloat(unit_amount / 100).toFixed(2)
 $('#remaining-quantity').textContent = `(${quantity} left)`
 $('#description').textContent = description
+$('#purchase-quantity').value = cart.getItems()[stripeProductId] || 1
